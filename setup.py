63c52923ad3c8854a33db77696897edf12d6593d
Jonathan Slenders <jonathan@slenders.be>
2014-09-09 12:53:03 +0200

    pypi release 0.9

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ from setuptools import setup, find_packages
 setup(
         name='prompt_toolkit',
         author='Jonathan Slenders',
-        version='0.8',
+        version='0.9',
         license='LICENSE.txt',
         url='https://github.com/jonathanslenders/python-prompt-toolkit',
 
