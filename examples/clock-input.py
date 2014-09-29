324bb5f9ae5a220f9bc17b68e9cfcc9cbf03e07d
Jonathan Slenders <jonathan@slenders.be>
2014-09-29 20:37:05 +0200

    Fixed the clock-input example.

--- examples/clock-input.py
+++ examples/clock-input.py
@@ -20,20 +20,25 @@ class ClockPrompt(Prompt):
         ]
 
 
-
-class ClockCLI(CommandLineInterface):
-    def on_read_input_start(self):
-        self.run_in_executor(self._clock_update)
-
-    def _clock_update(self):
-        # Send every second a redraw request.
-        while self.is_reading_input:
-            time.sleep(1)
-            self.request_redraw()
-
-
 def main():
-    cli = ClockCLI(layout=Layout(before_input=ClockPrompt()))
+    cli = CommandLineInterface(layout=Layout(before_input=ClockPrompt()))
+
+    def on_read_start():
+        """
+        This function is called when we start reading at the input.
+        (Actually the start of the read-input event loop.)
+        """
+        # Following function should be run in the background.
+        # We do it by using an executor thread from the `CommandLineInterface`
+        # instance.
+        def run():
+            # Send every second a redraw request.
+            while cli.is_reading_input:
+                time.sleep(1)
+                cli.request_redraw()
+
+        cli.run_in_executor(run)
+    cli.onReadInputStart += on_read_start
 
     code_obj = cli.read_input()
     print('You said: ' + code_obj.text)
