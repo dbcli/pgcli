2f6f0cdc6abd50c0dcc468a0492a0bd34b25d3dd
Jonathan Slenders <jonathan@slenders.be>
2014-09-29 20:46:44 +0200

    Pypi release 0.17.

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ from setuptools import setup, find_packages
 setup(
         name='prompt_toolkit',
         author='Jonathan Slenders',
-        version='0.16',
+        version='0.17',
         license='LICENSE.txt',
         url='https://github.com/jonathanslenders/python-prompt-toolkit',
 
