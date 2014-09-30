f53625f3585aca527970b287aee9684425e3991f
Christopher Welborn <cj@welbornprod.com>
2014-09-30 13:33:48 -0500

    Fix Issue #9, bad syntax causes jedi completions() to fail.

--- prompt_toolkit/contrib/python_input.py
+++ prompt_toolkit/contrib/python_input.py
@@ -446,9 +446,16 @@ class PythonCompleter(Completer):
         script = get_jedi_script_from_document(document, self.get_locals(), self.get_globals())
 
         if script:
-            for c in script.completions():
-                yield Completion(c.name_with_symbols, len(c.complete) - len(c.name_with_symbols),
-                                 display=c.name_with_symbols)
+            try:
+                completions = script.completions()
+            except TypeError:
+                # Issue #9: bad syntax causes completions() to fail in jedi.
+                # https://github.com/jonathanslenders/python-prompt-toolkit/issues/9
+                pass
+            else:
+                for c in completions:
+                    yield Completion(c.name_with_symbols, len(c.complete) - len(c.name_with_symbols),
+                                     display=c.name_with_symbols)
 
 
 class PythonCommandLineInterface(CommandLineInterface):
