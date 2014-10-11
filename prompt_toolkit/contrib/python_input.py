b4caab5361f28f68a6c2de2cd5a30a479f14ea72
Jonathan Slenders <jonathan@slenders.be>
2014-10-11 14:18:22 +0200

    Workaround for issue #43: UnicodeDecodeError on OpenBSD

--- prompt_toolkit/contrib/python_input.py
+++ prompt_toolkit/contrib/python_input.py
@@ -467,6 +467,10 @@ class PythonCompleter(Completer):
                 # Issue #9: bad syntax causes completions() to fail in jedi.
                 # https://github.com/jonathanslenders/python-prompt-toolkit/issues/9
                 pass
+            except UnicodeDecodeError:
+                # Issue #43: UnicodeDecodeError on OpenBSD
+                # https://github.com/jonathanslenders/python-prompt-toolkit/issues/43
+                pass
             else:
                 for c in completions:
                     yield Completion(c.name_with_symbols, len(c.complete) - len(c.name_with_symbols),
