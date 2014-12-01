5cc3dd0c91cf01f587258fdf0f89728e2a223cd5
Amjith Ramanujam <amjith@newrelic.com>
2014-11-30 22:05:24 -0800

    Update setup.py to read from the Readme.

--- setup.py
+++ setup.py
@@ -8,6 +8,9 @@ with open('pgcli/__init__.py', 'rb') as f:
     version = str(ast.literal_eval(_version_re.search(
         f.read().decode('utf-8')).group(1)))
 
+description = 'CLI for Postgres. With auto-completion and syntax highlighting.'
+
+
 setup(
         name='pgcli',
         author='Amjith Ramanujam',
@@ -15,8 +18,8 @@ setup(
         license='LICENSE.txt',
         url='https://github.com/amjith/pgcli',
         packages=find_packages(),
-        description='CLI for Postgres. With auto-completion and '
-                    'syntax highlighting',
+        description=description,
+        long_description=open('Readme.rst').read(),
         install_requires=[
             'Click',
             'prompt_toolkit',
@@ -29,8 +32,19 @@ setup(
             pgcli=pgcli.main:cli
         ''',
         classifiers=[
+            'Intended Audience :: Developers',
             'License :: OSI Approved :: BSD License',
+            'Operating System :: Unix',
             'Programming Language :: Python',
-            'Programming Language :: Python :: 3'
+            'Programming Language :: Python :: 2.6',
+            'Programming Language :: Python :: 2.7',
+            'Programming Language :: Python :: 3',
+            'Programming Language :: Python :: 3.3',
+            'Programming Language :: Python :: 3.4',
+            'Programming Language :: SQL',
+            'Topic :: Database',
+            'Topic :: Database :: Front-Ends',
+            'Topic :: Software Development',
+            'Topic :: Software Development :: Libraries :: Python Modules',
             ],
         )
