4ece22f9198810506f123df54c714e316002d949
Amjith Ramanujam <amjith@newrelic.com>
2014-12-01 07:59:59 -0800

    Add a lot more keywords to completion.

--- pgcli/pgcompleter.py
+++ pgcli/pgcompleter.py
@@ -3,19 +3,23 @@ from prompt_toolkit.completion import Completer, Completion
 import sqlparse
 
 class PGCompleter(Completer):
-    keywords = [
-        'SELECT',
-        'INSERT INTO',
-        'ALTER',
-        'DROP',
-        'DELETE FROM',
-        'FROM',
-        'BEGIN',
-        'TRANSACTION',
-        'ROLLBACK',
-        'VALUES',
-        'WHERE',
-    ]
+    keywords = ['ACCESS', 'ADD', 'ALL', 'ALTER', 'AND', 'ANY', 'AS', 'ASC',
+            'AUDIT', 'BETWEEN', 'BY', 'CHAR', 'CHECK', 'CLUSTER', 'COLUMN',
+            'COMMENT', 'COMPRESS', 'CONNECT', 'CREATE', 'CURRENT', 'DATE',
+            'DECIMAL', 'DEFAULT', 'DELETE', 'DESC', 'DISTINCT', 'DROP', 'ELSE',
+            'EXCLUSIVE', 'EXISTS', 'FILE', 'FLOAT', 'FOR', 'FROM', 'GRANT',
+            'GROUP', 'HAVING', 'IDENTIFIED', 'IMMEDIATE', 'IN', 'INCREMENT',
+            'INDEX', 'INITIAL', 'INSERT', 'INTEGER', 'INTERSECT', 'INTO', 'IS',
+            'LEVEL', 'LIKE', 'LOCK', 'LONG', 'MAXEXTENTS', 'MINUS', 'MLSLABEL',
+            'MODE', 'MODIFY', 'NOAUDIT', 'NOCOMPRESS', 'NOT', 'NOWAIT', 'NULL',
+            'NUMBER', 'OF', 'OFFLINE', 'ON', 'ONLINE', 'OPTION', 'OR', 'ORDER',
+            'PCTFREE', 'PRIOR', 'PRIVILEGES', 'PUBLIC', 'RAW', 'RENAME',
+            'RESOURCE', 'REVOKE', 'ROW', 'ROWID', 'ROWNUM', 'ROWS', 'SELECT',
+            'SESSION', 'SET', 'SHARE', 'SIZE', 'SMALLINT', 'START',
+            'SUCCESSFUL', 'SYNONYM', 'SYSDATE', 'TABLE', 'THEN', 'TO',
+            'TRIGGER', 'UID', 'UNION', 'UNIQUE', 'UPDATE', 'USER', 'VALIDATE',
+            'VALUES', 'VARCHAR', 'VARCHAR2', 'VIEW', 'WHENEVER', 'WHERE',
+            'WITH', ]
 
     special_commands = []
 
