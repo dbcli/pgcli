6a9b224834d1a523b03ce1e7c6ff4fa3ccea2583
Amjith Ramanujam <amjith@newrelic.com>
2014-12-23 23:29:26 -0800

    Add tests for parse_utils.extract_tables.

---
+++ tests/test_parse_utils.py
@@ -0,0 +1,29 @@
+from pgcli.packages.parseutils import extract_tables
+
+def test_simple_select_single_table():
+    tables = extract_tables('select * from abc')
+    assert tables == ['abc']
+
+def test_simple_select_multiple_tables():
+    tables = extract_tables('select * from abc, def')
+    assert tables == ['abc', 'def']
+
+def test_simple_select_with_cols_single_table():
+    tables = extract_tables('select a,b from abc')
+    assert tables == ['abc']
+
+def test_simple_select_with_cols_multiple_tables():
+    tables = extract_tables('select a,b from abc, def')
+    assert tables == ['abc', 'def']
+
+#def test_select_with_hanging_comma_single_table():
+    #tables = extract_tables('select a, from abc')
+    #assert tables == ['abc']
+
+#def test_select_with_hanging_comma_multiple_tables():
+    #tables = extract_tables('select a, from abc, def')
+    #assert tables == ['abc']
+
+#def test_simple_insert_single_table():
+    #tables = extract_tables('insert into abc (id, name) values (1, "def")')
+    #assert tables == ['abc']
