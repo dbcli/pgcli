f2ecb2ed6752fa6acd476c47f1e17eb4f0c2f051
Iryna Cherniavska <i.chernyavska@gmail.com>
2014-12-30 14:38:57 -0800

    Fix for bug #12: Config reading is failing when an option is missing.

--- pgcli/config.py
+++ pgcli/config.py
@@ -6,11 +6,19 @@ except ImportError:
     from configparser import ConfigParser
 # from prompt_toolkit.contrib.pdb import set_trace
 
-def load_config(filename):
+
+def load_config(filename, default_filename=None):
     filename = expanduser(filename)
     parser = ConfigParser()
+
+    # parser.read will not fail in case of IOError,
+    # so let's not try/except here.
+    if default_filename:
+        parser.read(default_filename)
+
     parser.read(filename)
     return parser
+
 
 def write_default_config(source, destination, overwrite=False):
     #import pdb; pdb.set_trace()
