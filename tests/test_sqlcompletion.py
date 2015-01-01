a4460f17cc6684023935afb904c07cb0be5d1550
Amjith Ramanujam <amjith@newrelic.com>
2015-01-01 00:16:43 -0800

    Remove the debug statment left in tests.

--- tests/test_sqlcompletion.py
+++ tests/test_sqlcompletion.py
@@ -66,7 +66,6 @@ def test_dot_suggests_cols_of_an_alias():
     assert suggestion == ('columns', ['tabl1'])
 
 def test_dot_col_comma_suggests_cols():
-    import pdb; pdb.set_trace()
     suggestion = suggest_type('SELECT t1.a, t2. FROM tabl1 t1, tabl2 t2',
             'SELECT t1.a, t2.')
     assert suggestion == ('columns', ['tabl2'])
