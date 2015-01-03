67a489f7856daa3b7df9a22578008670278befb9
Jonathan Slenders <jonathan@slenders.be>
2015-01-03 14:46:25 -0600

    Fixed Windows import errors.

--- prompt_toolkit/terminal/win32_input.py
+++ prompt_toolkit/terminal/win32_input.py
@@ -1,9 +1,9 @@
 from ctypes import windll, pointer, c_int
 from ctypes.wintypes import DWORD
 
-from ..keys import Keys
-from ..key_binding import KeyPress
-from ..win32_types import EventTypes, KEY_EVENT_RECORD, INPUT_RECORD, STD_INPUT_HANDLE
+from prompt_toolkit.key_binding.input_processor import KeyPress
+from prompt_toolkit.keys import Keys
+from prompt_toolkit.win32_types import EventTypes, KEY_EVENT_RECORD, INPUT_RECORD, STD_INPUT_HANDLE
 
 __all__ = (
     'ConsoleInputReader',
