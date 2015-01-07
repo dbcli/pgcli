decb370f88f2a33ca53c5edcc8ca5325157e267f
Stuart Quin <stuart.quin@gmail.com>
2015-01-07 10:30:48 +0000

    Add test for expected output

---
+++ tests/test_expanded.py
@@ -0,0 +1,14 @@
+from pgcli.packages.expanded import expanded_table
+import pytest
+
+def test_expanded_table_renders():
+    input = [("hello", 123),("world", 456)]
+
+    expected = """-[ RECORD 0 ]
+name | hello
+age  | 123
+-[ RECORD 1 ]
+name | world
+age  | 456
+"""
+    assert expected == expanded_table(input, ["name", "age"])
