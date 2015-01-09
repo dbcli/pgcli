498257cf37320da620c2650e10d08c47d3ae08d1
Karl-Aksel Puulmann <oxymaccy@gmail.com>
2015-01-09 12:40:55 +0200

    Fix expanded output package under python 3, add tests for unicode support.

--- pgcli/packages/expanded.py
+++ pgcli/packages/expanded.py
@@ -1,10 +1,12 @@
+from .tabulate import _text_type
+
 def pad(field, total, char=u" "):
     return field + (char * (total - len(field)))
 
 def get_separator(num, header_len, data_len):
     total_len = header_len + data_len + 1
 
-    sep = u"-[ RECORD {0} ]".format(unicode(num))
+    sep = u"-[ RECORD {0} ]".format(num)
     if len(sep) < header_len:
         sep = pad(sep, header_len - 1, u"-") + u"+"
 
@@ -22,13 +24,13 @@ def expanded_table(rows, headers):
     header_len += 2
 
     for row in rows:
-        row_len = max([len(str(x)) for x in row])
+        row_len = max([len(_text_type(x)) for x in row])
         row_result = ""
         if row_len > max_row_len:
             max_row_len = row_len
 
         for item in zip(padded_headers, row):
-            row_result += item[0] + u" " + unicode(item[1]) + u"\n"
+            row_result += u"{0} {1}\n".format(item[0], item[1])
 
         results.append(row_result)
 
@@ -38,4 +40,3 @@ def expanded_table(rows, headers):
         output += result
 
     return output
-
