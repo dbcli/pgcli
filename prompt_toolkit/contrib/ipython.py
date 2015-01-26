0827576d0359e28f540808adbc6c9c89ff503ba2
Jonathan Slenders <jonathan@slenders.be>
2015-01-26 10:18:41 +0100

    Backward compatibility fixes for ptpython/django_extensions.

--- prompt_toolkit/contrib/ipython.py
+++ prompt_toolkit/contrib/ipython.py
@@ -6,15 +6,16 @@ from __future__ import unicode_literals
 __all__ = ('PythonRepl', 'embed')
 
 
-def embed(*a, **kw):
-    """
-    DEPRECATED. Only for backwards compatibility.
-    Please call ptpython.ipython.embed directly!
-    """
-    try:
-        from ptpython.ipython import embed
-        embed(*a, **kw)
-    except ImportError as e:
-        print('prompt_toolkit was installed, but could not find ptpython.')
-        print('Please run: "pip install ptpython"')
-        raise e
+# We have this for compatibility with older versions of:
+# django_extensions.management.commands.shell_plus
+try:
+    import ptpython.ipython
+
+    def embed(*a, **kw):
+        """
+        DEPRECATED. Only for backwards compatibility.
+        Please call ptpython.ipython.embed directly!
+        """
+        ptpython.ipython.embed(*a, **kw)
+except ImportError as e:
+    pass
--- prompt_toolkit/contrib/repl.py
+++ prompt_toolkit/contrib/repl.py
@@ -6,15 +6,16 @@ from __future__ import unicode_literals
 __all__ = ('PythonRepl', 'embed')
 
 
-def embed(*a, **kw):
-    """
-    DEPRECATED. Only for backwards compatibility.
-    Please call ptpython.repl.embed directly!
-    """
-    try:
-        import ptpython.repl
+# We have this for compatibility with older versions of:
+# django_extensions.management.commands.shell_plus
+try:
+    import ptpython.repl
+
+    def embed(*a, **kw):
+        """
+        DEPRECATED. Only for backwards compatibility.
+        Please call ptpython.repl.embed directly!
+        """
         ptpython.repl.embed(*a, **kw)
-    except ImportError as e:
-        print('prompt_toolkit was installed, but could not find ptpython.')
-        print('Please run: "pip install ptpython"')
-        raise e
+except ImportError as e:
+    pass
