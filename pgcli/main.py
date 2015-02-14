2911aebbc9a06f6900cb3ccc18fe2be3b1efe5b3
Amjith Ramanujam <amjith.r@gmail.com>
2015-02-13 17:02:24 -0800

    Add a version flag.

--- pgcli/main.py
+++ pgcli/main.py
@@ -3,6 +3,7 @@ from __future__ import unicode_literals
 from __future__ import print_function
 
 import os
+import sys
 import traceback
 import logging
 from time import time
@@ -305,12 +306,19 @@ class PGCli(object):
         help='Force password prompt.')
 @click.option('-w', '--no-password', 'never_prompt', is_flag=True,
         default=False, help='Never prompt for password.')
+@click.option('-v', '--version', is_flag=True, help='Version of pgcli.')
 @click.option('-d', '--dbname', default='', envvar='PGDATABASE',
         help='database name to connect to.')
 @click.argument('database', default=lambda: None, envvar='PGDATABASE', nargs=1)
 @click.argument('username', default=lambda: None, envvar='PGUSER', nargs=1)
 def cli(database, user, host, port, prompt_passwd, never_prompt, dbname,
-        username):
+        username, version):
+
+    if version:
+        from .__init__ import __version__
+        print('Version:', __version__)
+        sys.exit(0)
+
     pgcli = PGCli(prompt_passwd, never_prompt)
 
     # Choose which ever one has a valid value.
