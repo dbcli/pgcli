37616d9927d888b742fced2e05ce63a2c5909e5b
Amjith Ramanujam <amjith.r@gmail.com>
2015-04-02 18:23:29 -0700

    Update release script.

--- release.py
+++ release.py
@@ -42,6 +42,11 @@ def create_source_tarball():
     print(' '.join(cmd))
     subprocess.check_output(cmd)
 
+def push_to_github():
+    cmd = ['git', 'push', 'origin']
+    print(' '.join(cmd))
+    subprocess.check_output(cmd)
+
 def push_tags_to_github():
     cmd = ['git', 'push', '--tags', 'origin']
     print(' '.join(cmd))
@@ -61,4 +66,5 @@ if __name__ == '__main__':
     create_git_tag('v%s' % ver)
     register_with_pypi()
     create_source_tarball()
+    push_to_github()
     push_tags_to_github()
