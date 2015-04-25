eebda4600c7ade3da6670f134fffc5afecdac209
Jonathan Slenders <jonathan@slenders.be>
2015-04-25 21:12:08 +0100

    Pypi release 0.33

--- setup.py
+++ setup.py
@@ -14,7 +14,7 @@ long_description = open(
 setup(
     name='prompt_toolkit',
     author='Jonathan Slenders',
-    version='0.32',
+    version='0.33',
     license='LICENSE.txt',
     url='https://github.com/jonathanslenders/python-prompt-toolkit',
     description='Library for building powerful interactive command lines in Python',
