8c51def7fadbf93319213ee0432695808a845543
Jonathan Slenders <jonathan@slenders.be>
2015-05-06 21:37:17 +0200

    Added WORD parameter to WordCompleter + small bugfix in WordCompleter.

--- prompt_toolkit/contrib/completers/base.py
+++ prompt_toolkit/contrib/completers/base.py
@@ -16,22 +16,20 @@ class WordCompleter(Completer):
     :param ignore_case: If True, case-insensitive completion.
     :param meta_dict: Optional dict mapping words to their meta-information.
     """
-    def __init__(self, words, ignore_case=False, meta_dict=None):
+    def __init__(self, words, ignore_case=False, meta_dict=None, WORD=False):
         self.words = list(words)
         self.ignore_case = ignore_case
         self.meta_dict = meta_dict or {}
+        self.WORD = WORD
         assert all(isinstance(w, string_types) for w in self.words)
 
-        if ignore_case:
-            self.words = [w.lower() for w in self.words]
-
     def get_completions(self, document, complete_event):
-        word_before_cursor = document.get_word_before_cursor()
+        word_before_cursor = document.get_word_before_cursor(WORD=self.WORD)
 
         if self.ignore_case:
             word_before_cursor = word_before_cursor.lower()
 
         for a in self.words:
-            if a.startswith(word_before_cursor):
+            if a.lower().startswith(word_before_cursor):
                 display_meta = self.meta_dict.get(a, '')
                 yield Completion(a, -len(word_before_cursor), display_meta=display_meta)
