f26ca1ee03fefcd1a992608cec6ac4f9bb499875
Jonathan Slenders <jonathan@slenders.be>
2015-06-02 17:36:49 +0200

    Bugfix in Windows eventloop. HANDLE is not a c_long on 64bit Python. Use HANDLE type instead.

--- prompt_toolkit/eventloop/win32.py
+++ prompt_toolkit/eventloop/win32.py
@@ -10,8 +10,8 @@ from ..terminal.win32_input import ConsoleInputReader
 from ..win32_types import SECURITY_ATTRIBUTES
 from .base import EventLoop, INPUT_TIMEOUT
 
-from ctypes import windll, pointer, c_long
-from ctypes.wintypes import DWORD, BOOL
+from ctypes import windll, pointer
+from ctypes.wintypes import DWORD, BOOL, HANDLE
 
 import threading
 
@@ -109,9 +109,11 @@ def _wait_for_handles(handles, timeout=-1):
 
     http://msdn.microsoft.com/en-us/library/windows/desktop/ms687025(v=vs.85).aspx
     """
-    arrtype = c_long * len(handles)
+    arrtype = HANDLE * len(handles)
     handle_array = arrtype(*handles)
-    ret = windll.kernel32.WaitForMultipleObjects(len(handle_array), handle_array, False, DWORD(timeout))
+
+    ret = windll.kernel32.WaitForMultipleObjects(
+        len(handle_array), handle_array, BOOL(False), DWORD(timeout))
 
     if ret == WAIT_TIMEOUT:
         return None
