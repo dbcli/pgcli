d98a90e0ff41819e19ea245fcbdd06f573f2cb4c
Jonathan Slenders <jonathan@slenders.be>
2015-06-11 18:44:36 +0200

    Added SwitchableValidator class.

--- prompt_toolkit/validation.py
+++ prompt_toolkit/validation.py
@@ -1,11 +1,17 @@
 """
+Input validation for a `Buffer`.
+(Validators will be called before accepting input.)
 """
 from __future__ import unicode_literals
+from .filters import SimpleFilter, Always
+
 from abc import ABCMeta, abstractmethod
 from six import with_metaclass
 
 __all__ = (
+    'SwitchableValidator',
     'ValidationError',
+    'Validator',
 )
 
 
@@ -28,3 +34,21 @@ class Validator(with_metaclass(ABCMeta, object)):
         If invalid, this should raise `self.validation_error`.
         """
         pass
+
+
+class SwitchableValidator(Validator):
+    """
+    Validator that can be switched on/off according to
+    a filter. (This wraps around another validator.)
+    """
+    def __init__(self, validator, enabled=Always()):
+        assert isinstance(validator, Validator)
+        assert isinstance(enabled, SimpleFilter)
+
+        self.validator = validator
+        self.enabled = enabled
+
+    def validate(self, document):
+        # Call the validator only if the filter is active.
+        if self.enabled():
+            self.validator.validate(document)
