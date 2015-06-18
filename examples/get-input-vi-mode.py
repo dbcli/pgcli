c7b38d3f54b05f30d13e56f7cce8b3d2cf801463
Jonathan Slenders <jonathan@slenders.be>
2015-06-18 20:34:05 +0200

    Fixed examples/get-input-vi-mode.py

--- examples/get-input-vi-mode.py
+++ examples/get-input-vi-mode.py
@@ -1,9 +1,10 @@
 #!/usr/bin/env python
 from __future__ import unicode_literals
 from prompt_toolkit.shortcuts import get_input
+from prompt_toolkit.filters import Always
 
 
 if __name__ == '__main__':
     print("You have Vi keybindings here. Press [Esc] to go to navigation mode.")
-    answer = get_input('Give me some input: ', multiline=False, vi_mode=True)
+    answer = get_input('Give me some input: ', multiline=False, vi_mode=Always())
     print('You said: %s' % answer)
