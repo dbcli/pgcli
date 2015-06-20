1cd5ff0afe8601be9392c09ba16289609ba32f97
Jonathan Slenders <jonathan@slenders.be>
2015-06-20 18:18:45 +0200

    Added win32_refresh_window function for fixing Win32 redraw issues. (Not yet called)

--- prompt_toolkit/terminal/win32_output.py
+++ prompt_toolkit/terminal/win32_output.py
@@ -1,6 +1,6 @@
 from __future__ import unicode_literals
 
-from ctypes import windll, byref, ArgumentError, c_char, c_long, c_ulong
+from ctypes import windll, byref, ArgumentError, c_char, c_long, c_ulong, c_uint
 from ctypes.wintypes import DWORD
 
 from ..win32_types import CONSOLE_SCREEN_BUFFER_INFO, STD_OUTPUT_HANDLE, COORD, SMALL_RECT
@@ -248,6 +248,21 @@ class Win32Output(Output):
 
     def quit_alternate_screen(self):
         pass
+
+    @classmethod
+    def win32_refresh_window(cls):
+        """
+        Call win32 API to refresh the whole Window.
+
+        This is sometimes necessary when the application paints background
+        for completion menus. When the menu disappears, it leaves traces due
+        to a bug in the Windows Console. Sending a repaint request solves it.
+        """
+        # Get console handle
+        handle = windll.kernel32.GetConsoleWindow()
+
+        RDW_INVALIDATE = 0x0001
+        windll.user32.RedrawWindow(handle, None, None, c_uint(RDW_INVALIDATE))
 
 
 class FOREGROUND_COLOR:
