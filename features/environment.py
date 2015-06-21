ba686f0d3368367294f7c2edc78f600b60272f9f
Iryna Cherniavska <i.chernyavska@gmail.com>
2015-06-21 15:00:18 -0700

    Started working on integration tests.

---
+++ features/environment.py
@@ -0,0 +1,71 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+from __future__ import print_function
+
+import os
+import pexpect
+import db_utils as dbutils
+
+
+def before_all(context):
+    """
+    Set env parameters.
+    """
+    os.environ['LINES'] = "50"
+    os.environ['COLUMNS'] = "120"
+    context.exit_sent = False
+
+    # Store get params from config.
+    context.conf = {
+        'host': context.config.userdata.get('pg_test_host', 'localhost'),
+        'user': context.config.userdata.get('pg_test_user', 'root'),
+        'pass': context.config.userdata.get('pg_test_pass', None),
+        'dbname': context.config.userdata.get('pg_test_db', None),
+    }
+
+    # Store old env vars.
+    context.pgenv = {
+        'PGDATABASE': os.environ.get('PGDATABASE', None),
+        'PGUSER': os.environ.get('PGUSER', None),
+        'PGHOST': os.environ.get('PGHOST', None),
+        'PGPASS': os.environ.get('PGPASS', None),
+    }
+
+    # Set new env vars.
+    os.environ['PGDATABASE'] = context.conf['dbname']
+    os.environ['PGUSER'] = context.conf['user']
+    os.environ['PGHOST'] = context.conf['host']
+    if context.conf['pass']:
+        os.environ['PGPASS'] = context.conf['pass']
+    elif 'PGPASS' in os.environ:
+        del os.environ['PGPASS']
+
+    context.cn = dbutils.create_db(context.conf['host'], context.conf['user'],
+                                   context.conf['pass'], context.conf['dbname'])
+
+
+def after_all(context):
+    """
+    Unset env parameters.
+    """
+    dbutils.close_cn(context.cn)
+    dbutils.drop_db(context.conf['host'], context.conf['user'],
+                    context.conf['pass'], context.conf['dbname'])
+
+    # Restore env vars.
+    for k, v in context.pgenv.items():
+        if k in os.environ and v is None:
+            del os.environ[k]
+        elif v:
+            os.environ[k] = v
+
+
+def after_scenario(context, _):
+    """
+    Cleans up after each test complete.
+    """
+
+    if hasattr(context, 'cli') and not context.exit_sent:
+        # Send Ctrl + D into cli
+        context.cli.sendcontrol('d')
+        context.cli.expect(pexpect.EOF)
---
+++ features/simple_cli_commands.feature
@@ -0,0 +1,15 @@
+Feature: run the cli,
+  call the help command,
+  exit the cli
+
+  Scenario: run the cli
+     Given we have pgcli installed
+      when we run pgcli
+      then we see pgcli prompt
+
+  Scenario: run the cli and exit
+     Given we have pgcli installed
+      when we run pgcli
+      and we wait for prompt
+      and we send "ctrl + d"
+      then pgcli exits
