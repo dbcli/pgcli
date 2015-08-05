700807c3e958278f617d9e4b6d3e1486e141cfda
Jonathan Slenders <jonathan@slenders.be>
2015-08-05 04:21:44 +0200

    Import all modules for syntax checking in unit tests.

--- tests/run_tests.py
+++ tests/run_tests.py
@@ -11,5 +11,43 @@ from layout_tests import *
 
 import unittest
 
+# Import modules for syntax checking.
+import prompt_toolkit
+import prompt_toolkit.application
+import prompt_toolkit.buffer
+import prompt_toolkit.clipboard
+import prompt_toolkit.completion
+import prompt_toolkit.contrib.completers
+import prompt_toolkit.contrib.regular_languages
+import prompt_toolkit.contrib.telnet
+import prompt_toolkit.contrib.validators
+import prompt_toolkit.document
+import prompt_toolkit.enums
+import prompt_toolkit.eventloop.base
+import prompt_toolkit.eventloop.inputhook
+import prompt_toolkit.eventloop.posix
+import prompt_toolkit.eventloop.posix_utils
+import prompt_toolkit.eventloop.utils
+import prompt_toolkit.focus_stack
+import prompt_toolkit.history
+import prompt_toolkit.input
+import prompt_toolkit.interface
+import prompt_toolkit.key_binding
+import prompt_toolkit.keys
+import prompt_toolkit.layout
+import prompt_toolkit.libs
+import prompt_toolkit.output
+import prompt_toolkit.reactive
+import prompt_toolkit.renderer
+import prompt_toolkit.search_state
+import prompt_toolkit.selection
+import prompt_toolkit.shortcuts
+import prompt_toolkit.styles
+import prompt_toolkit.terminal
+import prompt_toolkit.terminal.vt100_input
+import prompt_toolkit.terminal.vt100_output
+import prompt_toolkit.utils
+import prompt_toolkit.validation
+
 if __name__ == '__main__':
     unittest.main()
