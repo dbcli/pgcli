7a31da6a781e5fb43630595af302fdb9be4f579a
Jonathan Slenders <jonathan@slenders.be>
2015-08-08 20:44:07 +0200

    Fix: Correct invalidation of DefaultPrompt when search direction changes.

--- prompt_toolkit/layout/prompt.py
+++ prompt_toolkit/layout/prompt.py
@@ -75,6 +75,7 @@ class DefaultPrompt(Processor):
         return (
             cli.input_processor.arg,
             cli.is_searching,
+            cli.is_searching and cli.search_state.direction,
             cli.buffers[SEARCH_BUFFER].text,
             self.get_tokens(cli),
         )
