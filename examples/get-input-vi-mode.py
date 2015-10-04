03efdbfcb4cb4410e2b9386be292571d78a5f92b
Jonathan Slenders <jonathan@slenders.be>
2015-10-05 01:41:32 +0200

    Use True/False instead of Always/Never.

--- examples/get-input-vi-mode.py
+++ examples/get-input-vi-mode.py
@@ -1,10 +1,9 @@
 #!/usr/bin/env python
 from __future__ import unicode_literals
 from prompt_toolkit import prompt
-from prompt_toolkit.filters import Always
 
 
 if __name__ == '__main__':
     print("You have Vi keybindings here. Press [Esc] to go to navigation mode.")
-    answer = prompt('Give me some input: ', multiline=False, vi_mode=Always())
+    answer = prompt('Give me some input: ', multiline=False, vi_mode=True)
     print('You said: %s' % answer)
