a47546e369336f52d1a5cf059ff4ef1dc4a6a3a7
Jonathan Slenders <jonathan@slenders.be>
2015-10-14 18:35:25 +0200

    Renamed the actual value of the SEARCH_BUFFER, DEFAULT_BUFFER, SYSTEM_BUFFER and DUMMY_BUFFER, as well as IncrementalSearchDirection.FORWARD/BACKWARD.

--- prompt_toolkit/enums.py
+++ prompt_toolkit/enums.py
@@ -2,22 +2,22 @@ from __future__ import unicode_literals
 
 
 class IncrementalSearchDirection:
-    FORWARD = 'forward'
-    BACKWARD = 'backward'
+    FORWARD = 'FORWARD'
+    BACKWARD = 'BACKWARD'
 
 
 #: Name of the search buffer.
-SEARCH_BUFFER = 'search'
+SEARCH_BUFFER = 'SEARCH_BUFFER'
 
 #: Name of the default buffer.
-DEFAULT_BUFFER = 'default'
+DEFAULT_BUFFER = 'DEFAULT_BUFFER'
 
 #: Name of the system buffer.
-SYSTEM_BUFFER = 'system'
+SYSTEM_BUFFER = 'SYSTEM_BUFFER'
 
 # Dummy buffer. This is the buffer returned by
 # `CommandLineInterface.current_buffer` when the top of the `FocusStack` is
 # `None`. This could be the case when there is some widget has the focus and no
 # actual text editing is possible. This buffer should also never be displayed.
 # (It will never contain any actual text.)
-DUMMY_BUFFER = 'dummy'
+DUMMY_BUFFER = 'DUMMY_BUFFER'
