1ff4a510500ba073b75e4f86a6b95941da8a3c57
Jonathan Slenders <jonathan@slenders.be>
2016-01-03 04:15:59 +0100

    Removed unused import.

--- prompt_toolkit/contrib/completers/system.py
+++ prompt_toolkit/contrib/completers/system.py
@@ -3,8 +3,6 @@ from __future__ import unicode_literals
 from prompt_toolkit.contrib.regular_languages.completion import GrammarCompleter
 from prompt_toolkit.contrib.regular_languages.compiler import compile
 
-import os
-
 from .filesystem import PathCompleter, ExecutableCompleter
 
 __all__ = (
