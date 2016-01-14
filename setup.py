af8715670759142a50036e015b7f46e1591287ea
Adam Chainz <adam@adamj.eu>
2016-01-14 22:32:41 +0000

    Python 3.5 Support
    Test on Travis and report it to PyPI

--- setup.py
+++ setup.py
@@ -46,6 +46,7 @@ setup(
         'Programming Language :: Python :: 3',
         'Programming Language :: Python :: 3.3',
         'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
         'Programming Language :: Python',
         'Topic :: Software Development',
     ],
