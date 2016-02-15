afaa95781e297c55da49bfea96e01f0deaea40d9
Jonathan Slenders <jonathan@slenders.be>
2016-02-15 21:02:33 +0100

    Fixed syntax error in conemu_output.

--- prompt_toolkit/terminal/conemu_output.py
+++ prompt_toolkit/terminal/conemu_output.py
@@ -33,7 +33,7 @@ class ConEmuOutput(object):
         if name in ('get_size', 'get_rows_below_cursor_position',
                     'enable_mouse_support', 'disable_mouse_support',
                     'scroll_buffer_to_prompt', 'get_win32_screen_buffer_info',
-                    'enable_bracketed_paste', 'disable_bracketed_paste):
+                    'enable_bracketed_paste', 'disable_bracketed_paste'):
             return getattr(self.win32_output, name)
         else:
             return getattr(self.vt100_output, name)
