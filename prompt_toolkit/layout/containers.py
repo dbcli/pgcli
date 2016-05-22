4b835f785efb0a15d253ecdee2cade518fcef0b7
Jonathan Slenders <jonathan@slenders.be>
2016-05-22 11:48:32 +0200

    Better feedback when entering multi-key key bindings in insert mode. (E.g. when 'jj' would be mapped to escape.)

--- prompt_toolkit/layout/containers.py
+++ prompt_toolkit/layout/containers.py
@@ -14,7 +14,7 @@ from .margins import Margin
 from .screen import Point, WritePosition, _CHAR_CACHE
 from .utils import token_list_to_text, explode_tokens
 from prompt_toolkit.cache import SimpleCache
-from prompt_toolkit.filters import to_cli_filter
+from prompt_toolkit.filters import to_cli_filter, ViInsertMode, EmacsInsertMode
 from prompt_toolkit.mouse_events import MouseEvent, MouseEventType
 from prompt_toolkit.reactive import Integer
 from prompt_toolkit.token import Token
@@ -834,6 +834,9 @@ class ColorColumn(object):
         self.token = token
 
 
+_in_insert_mode = ViInsertMode() | EmacsInsertMode()
+
+
 class Window(Container):
     """
     Container that holds a control.
@@ -1306,6 +1309,11 @@ class Window(Container):
                     cli, new_screen, screen_cursor_position, xpos, ypos, width,
                     write_position.height)
 
+        # Draw input characters from the input processor queue.
+        if has_focus and ui_content.cursor_position:
+            self._show_input_processor_key_buffer(cli, new_screen)
+
+        # Set menu position.
         if not new_screen.menu_position and ui_content.menu_position:
             new_screen.menu_position = cursor_pos_to_screen_pos(
                     ui_content.menu_position.y, ui_content.menu_position.x)
@@ -1325,6 +1333,27 @@ class Window(Container):
             cpos = new_screen.cursor_position
             new_screen.data_buffer[cpos.y][cpos.x] = \
                 _CHAR_CACHE[digraph_char, Token.Digraph]
+
+    def _show_input_processor_key_buffer(self, cli, new_screen):
+        """
+        When the user is typing a key binding that consists of several keys,
+        display the last pressed key if the user is in insert mode and the key
+        is meaningful to be displayed.
+        E.g. Some people want to bind 'jj' to escape in Vi insert mode. But the
+             first 'j' needs to be displayed in order to get some feedback.
+        """
+        key_buffer = cli.input_processor.key_buffer
+
+        if key_buffer and _in_insert_mode(cli):
+            # The textual data for the given key. (Can be a VT100 escape
+            # sequence.)
+            data = key_buffer[-1].data
+
+            # Display only if this is a 1 cell width character.
+            if get_cwidth(data) == 1:
+                cpos = new_screen.cursor_position
+                new_screen.data_buffer[cpos.y][cpos.x] = \
+                    _CHAR_CACHE[data, Token.PartialKeyBinding]
 
     def _highlight_cursorlines(self, cli, new_screen, cpos, x, y, width, height):
         """
