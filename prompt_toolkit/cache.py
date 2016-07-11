6a852f09de1ef28524742c4f37e9c0e180657792
Jonathan Slenders <jonathan@slenders.be>
2016-07-11 22:22:07 +0200

    Added memoized decorator.

--- prompt_toolkit/cache.py
+++ prompt_toolkit/cache.py
@@ -1,9 +1,11 @@
 from __future__ import unicode_literals
 from collections import deque
+from functools import wraps
 
 __all__ = (
     'SimpleCache',
     'FastDictCache',
+    'memoized',
 )
 
 
@@ -89,3 +91,21 @@ class FastDictCache(dict):
         self[key] = result
         self._keys.append(key)
         return result
+
+
+def memoized(maxsize=1024):
+    """
+    Momoization decorator for immutable classes and pure functions.
+    """
+    cache = SimpleCache(maxsize=maxsize)
+
+    def decorator(obj):
+        @wraps(obj)
+        def new_callable(*a, **kw):
+            def create_new():
+                return obj(*a, **kw)
+
+            key = (a, tuple(kw.items()))
+            return cache.get(key, create_new)
+        return new_callable
+    return decorator
