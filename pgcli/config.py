5c6be4513e00904bb808f16643dca013eb9b57bb
Fabien Meghazi <agr@amigrave.com>
2016-09-13 22:47:35 +0200

    XDG_CONFIG_HOME support for config file location

--- pgcli/config.py
+++ pgcli/config.py
@@ -8,6 +8,8 @@ from configobj import ConfigObj
 def config_location():
     if platform.system() == 'Windows':
         return os.getenv('USERPROFILE') + '\\AppData\\Local\\dbcli\\pgcli\\'
+    elif 'XDG_CONFIG_HOME' in os.environ:
+        return '%s/pgcli/' % expanduser(os.environ['XDG_CONFIG_HOME'])
     else:
         return expanduser('~/.config/pgcli/')
 
