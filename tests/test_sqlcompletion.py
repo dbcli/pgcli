279d6e85a78b18bd94941e7159de9c7d7b0acd6e
Joakim Koljonen <koljonen@outlook.com>
2016-11-20 21:18:40 +0100

    Fix crash after `with`

--- tests/test_sqlcompletion.py
+++ tests/test_sqlcompletion.py
@@ -20,6 +20,12 @@ def test_select_suggests_cols_with_qualified_table_scope():
     assert set(suggestions) == cols_etc('tabl', 'sch')
 
 
+def test_cte_does_not_crash():
+    sql = 'WITH CTE AS (SELECT F.* FROM Foo F WHERE F.Bar > 23) SELECT C.* FROM CTE C WHERE C.FooID BETWEEN 123 AND 234;'
+    for i in range(len(sql)):
+        suggestions = suggest_type(sql[:i+1], sql[:i+1])
+
+
 @pytest.mark.parametrize('expression', [
     'SELECT * FROM "tabl" WHERE ',
 ])
