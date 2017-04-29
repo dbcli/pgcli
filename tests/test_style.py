f56057daccc97e660be977b972c213fce908cccd
Jonathan Slenders <jonathan@slenders.be>
2017-04-29 18:22:12 +0200

    Don't use OrderedDict. Not supported on Python 2.6.

--- tests/test_style.py
+++ tests/test_style.py
@@ -1,7 +1,5 @@
 from __future__ import unicode_literals
-
 from prompt_toolkit.styles import Attrs, Style
-from collections import OrderedDict
 
 
 def test_style_from_dict():
@@ -38,11 +36,11 @@ def test_style_from_dict():
 def test_class_combinations_1():
     # In this case, our style has both class 'a' and 'b'.
     # Given that the style for 'a b' is defined at the end, that one is used.
-    style = Style.from_dict(OrderedDict([
+    style = Style([
         ('a', '#0000ff'),
         ('b', '#00ff00'),
         ('a b', '#ff0000'),
-    ]))
+    ])
     expected = Attrs(color='ff0000', bgcolor='', bold=False,
                      underline=False, italic=False, blink=False, reverse=False)
     assert style.get_attrs_for_style_str('class:a class:b') == expected
@@ -57,11 +55,11 @@ def test_class_combinations_1():
 def test_class_combinations_2():
     # In this case, our style has both class 'a' and 'b'.
     # The style that is defined the latest get priority.
-    style = Style.from_dict(OrderedDict([
+    style = Style([
         ('a b', '#ff0000'),
         ('b', '#00ff00'),
         ('a', '#0000ff'),
-    ]))
+    ])
     expected = Attrs(color='00ff00', bgcolor='', bold=False,
                      underline=False, italic=False, blink=False, reverse=False)
     assert style.get_attrs_for_style_str('class:a class:b') == expected
@@ -76,12 +74,12 @@ def test_class_combinations_2():
 
 
 def test_substyles():
-    style = Style.from_dict(OrderedDict([
+    style = Style([
         ('a.b', '#ff0000 bold'),
         ('a', '#0000ff'),
         ('b', '#00ff00'),
         ('b.c', '#0000ff italic'),
-    ]))
+    ])
 
     # Starting with a.*
     expected = Attrs(color='0000ff', bgcolor='', bold=False,
