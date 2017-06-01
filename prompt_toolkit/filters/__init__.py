64a08cb85594cb217775adad00177a4f4eb2cb15
Jonathan Slenders <jonathan@slenders.be>
2017-06-01 22:20:07 +0200

    Removed old filters/types.py file.

--- prompt_toolkit/filters/__init__.py
+++ prompt_toolkit/filters/__init__.py
@@ -11,28 +11,15 @@ state of what they are observing. Only when a filter is called (it's actually a
 callable), it will calculate its value. So, its not really reactive
 programming, but it's made to fit for this framework.
 
-One class of filters observe an `Application` instance. However, they are not
-attached to such an instance. (We have to pass this instance to the filter when
-calling it.) The reason for this is to allow declarative programming: for key
-bindings, we can attach a filter to a key binding without knowing yet which
-`Application` instance it will observe in the end.  Examples are `HasSearch` or
-`IsExiting`.
-
-Another class of filters doesn't take anything as input. And a third class of
-filters are universal, for instance `Always` and `Never`.
-It is impossible to mix the first and the second class, because that would mean
-mixing filters with a different signature.
-
 Filters can be chained using ``&`` and ``|`` operations, and inverted using the
 ``~`` operator, for instance::
 
-    filter = HasFocus('default') & ~ HasSelection()
+    filter = has_focus('default') & ~ has_selection
 """
 from __future__ import unicode_literals
 
 from .base import *
 from .app import *
-from .types import *
 from .utils import *
 
 # For backwards-compatibility. Keep this import.
