bd582de288b39b4368aee0bb998c7838941c5f0b
Jonathan Slenders <jonathan@slenders.be>
2017-07-16 12:37:47 +0200

    Fixed terminal-title.py example.

--- examples/prompts/terminal-title.py
+++ examples/prompts/terminal-title.py
@@ -1,11 +1,12 @@
 #!/usr/bin/env python
 from __future__ import unicode_literals
 from prompt_toolkit import prompt
+from prompt_toolkit.shortcuts import set_title
 
 
 if __name__ == '__main__':
-    def get_title():
-        return 'This is the title'
+    set_title('This is the terminal title')
+    answer = prompt('Give me some input: ')
+    set_title('')
 
-    answer = prompt('Give me some input: ', get_title=get_title)
     print('You said: %s' % answer)
