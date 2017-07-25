1624cf608dee5aef5198077c3574743d9861cf33
Irina Truong <i.chernyavska@gmail.com>
2017-07-25 10:16:34 -0700

    Behave tests should also accept pgport.

--- tests/features/environment.py
+++ tests/features/environment.py
@@ -46,12 +46,22 @@ def before_all(context):
             'pg_test_pass',
             os.getenv('PGPASSWORD', None)
         ),
+        'port': context.config.userdata.get(
+            'pg_test_port',
+            os.getenv('PGPORT', '5432')
+        ),
+        'cli_command': (
+            context.config.userdata.get('pg_cli_command', None) or
+            '{python} -c "{startup}"'.format(
+                python=sys.executable,
+                startup='; '.join([
+                    "import coverage",
+                    "coverage.process_startup()",
+                    "import pgcli.main",
+                    "pgcli.main.cli()"]))),
         'dbname': db_name_full,
         'dbname_tmp': db_name_full + '_tmp',
         'vi': vi,
-        'cli_command': context.config.userdata.get('pg_cli_command', None) or
-                       sys.executable +
-        ' -c "import coverage; coverage.process_startup(); import pgcli.main; pgcli.main.cli()"',
         'pager_boundary': '---boundary---',
     }
     os.environ['PAGER'] = "{0} {1} {2}".format(
@@ -65,24 +75,24 @@ def before_all(context):
         'PGUSER': os.environ.get('PGUSER', None),
         'PGHOST': os.environ.get('PGHOST', None),
         'PGPASSWORD': os.environ.get('PGPASSWORD', None),
+        'PGPORT': os.environ.get('PGPORT', None),
     }
 
     # Set new env vars.
     os.environ['PGDATABASE'] = context.conf['dbname']
     os.environ['PGUSER'] = context.conf['user']
     os.environ['PGHOST'] = context.conf['host']
+    os.environ['PGPORT'] = context.conf['port']
 
     if context.conf['pass']:
         os.environ['PGPASSWORD'] = context.conf['pass']
     else:
         if 'PGPASSWORD' in os.environ:
             del os.environ['PGPASSWORD']
-        if 'PGHOST' in os.environ:
-            del os.environ['PGHOST']
 
     context.cn = dbutils.create_db(context.conf['host'], context.conf['user'],
-                                   context.conf['pass'],
-                                   context.conf['dbname'])
+                                   context.conf['pass'], context.conf['dbname'],
+                                   context.conf['port'])
 
     context.fixture_data = fixutils.read_fixture_files()
 
@@ -93,7 +103,8 @@ def after_all(context):
     """
     dbutils.close_cn(context.cn)
     dbutils.drop_db(context.conf['host'], context.conf['user'],
-                    context.conf['pass'], context.conf['dbname'])
+                    context.conf['pass'], context.conf['dbname'],
+                    context.conf['port'])
 
     # Restore env vars.
     for k, v in context.pgenv.items():
