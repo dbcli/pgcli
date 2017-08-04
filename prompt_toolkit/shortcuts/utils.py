0f9cf4b723c6ef2c6eaf3e1b0d127fcf1c918389
Jonathan Slenders <jonathan@slenders.be>
2017-08-05 01:36:52 +0200

    Python 2.6 compatibility.

--- prompt_toolkit/shortcuts/utils.py
+++ prompt_toolkit/shortcuts/utils.py
@@ -66,7 +66,7 @@ def print(*values, **kwargs):
     # Merges values.
     def to_text(val):
         if isinstance(val, list):
-            return to_formatted_text('{}'.format(val))
+            return to_formatted_text('{0}'.format(val))
         return to_formatted_text(val, auto_convert=True)
 
     fragments = []
