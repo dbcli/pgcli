dc680e1b8f4e93fb5b72af6440428445199c05e6
Jonathan Slenders <jonathan@slenders.be>
2017-11-11 19:12:34 +0100

    Added styled-apt-get-install.py example.

---
+++ examples/progress-bar/styled-apt-get-install.py
@@ -0,0 +1,34 @@
+#!/usr/bin/env python
+"""
+Styled just like an apt-get installation.
+"""
+from __future__ import unicode_literals
+from prompt_toolkit.shortcuts.progress_bar import progress_bar
+from prompt_toolkit.styles import Style
+from prompt_toolkit.shortcuts.progress_bar import formatters
+import time
+
+style = Style.from_dict({
+    'percentage': 'bg:#ffff00 #000000',
+    'current': '#448844',
+    'bar': '',
+})
+
+
+def main():
+    custom_formatters = [
+        formatters.Text('Progress: [', style='class:percentage'),
+        formatters.Percentage(),
+        formatters.Text(']', style='class:percentage'),
+        formatters.Text(' '),
+        formatters.Bar(sym_a='#', sym_b='#', sym_c='.'),
+        formatters.Text('  '),
+    ]
+
+    with progress_bar(style=style, formatters=custom_formatters) as pb:
+        for i in pb(range(1600), task_name='Installing'):
+            time.sleep(.01)
+
+
+if __name__ == '__main__':
+    main()
