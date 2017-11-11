7595c5adaf39e2094c135163dd6c9187e13d6f03
Jonathan Slenders <jonathan@slenders.be>
2017-11-11 19:53:30 +0100

    Aedded styled-tqdm, a progress bar styling, just like tqdm.

---
+++ examples/progress-bar/styled-tqdm.py
@@ -0,0 +1,41 @@
+#!/usr/bin/env python
+"""
+Styled similar to tqdm, another progress bar implementation in Python.
+
+See: https://github.com/noamraph/tqdm
+"""
+from __future__ import unicode_literals
+from prompt_toolkit.shortcuts.progress_bar import progress_bar
+from prompt_toolkit.styles import Style
+from prompt_toolkit.shortcuts.progress_bar import formatters
+import time
+
+style = Style.from_dict({
+    '': 'cyan',
+})
+
+
+def main():
+    custom_formatters = [
+        formatters.Bar(start='|', end='|', sym_a='#', sym_b='#', sym_c='-'),
+        formatters.Text(' '),
+        formatters.Progress(),
+        formatters.Text(' '),
+        formatters.Percentage(),
+        formatters.Text(' [elapsed: '),
+        formatters.TimeElapsed(),
+        formatters.Text(' left: '),
+        formatters.TimeLeft(),
+        formatters.Text(', '),
+        formatters.IterationsPerSecond(),
+        formatters.Text(' iters/sec]'),
+        formatters.Text('  '),
+    ]
+
+    with progress_bar(style=style, formatters=custom_formatters) as pb:
+        for i in pb(range(1600), task_name='Installing'):
+            time.sleep(.01)
+
+
+if __name__ == '__main__':
+    main()
