e6c90194f4c2826e5b144ab0025c8827225adbaf
Jonathan Slenders <jonathan@slenders.be>
2017-12-21 22:12:32 +0100

    Added a 'style_from_pygments_dict' class.

--- prompt_toolkit/styles/pygments.py
+++ prompt_toolkit/styles/pygments.py
@@ -11,6 +11,7 @@ from .style import Style
 
 __all__ = (
     'style_from_pygments',
+    'style_from_pygments_dict',
     'pygments_token_to_classname',
 )
 
@@ -32,11 +33,19 @@ def style_from_pygments(pygments_style_cls=None):
     from pygments.style import Style as pygments_Style
     assert issubclass(pygments_style_cls, pygments_Style)
 
+    return style_from_pygments_dict(pygments_style_cls.styles)
+
+
+def style_from_pygments_dict(pygments_dict):
+    """
+    Create a :class:`.Style` instance from a Pygments style dictionary.
+    (One that maps Token objects to style strings.)
+    """
+    assert isinstance(pygments_dict, dict)
     pygments_style = []
 
-    if pygments_style_cls is not None:
-        for token, style in pygments_style_cls.styles.items():
-            pygments_style.append((pygments_token_to_classname(token), style))
+    for token, style in pygments_dict.items():
+        pygments_style.append((pygments_token_to_classname(token), style))
 
     return Style(pygments_style)
 
