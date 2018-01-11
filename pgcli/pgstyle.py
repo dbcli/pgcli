f5f28dd8c72b9749ab576ae6546ef0128ae1af43
fraoustin <fraoustin@gmail.com>
2018-01-11 21:40:45 +0100

    correction of my mistake ... revert -2

--- pgcli/pgstyle.py
+++ pgcli/pgstyle.py
@@ -10,12 +10,8 @@ def style_factory(name, cli_style):
     except ClassNotFound:
         style = pygments.styles.get_style_by_name('native')
 
-    custom_styles = {}
-    for token in cli_style:
-        try:
-            custom_styles[string_to_tokentype(
-                token)] = style.styles[string_to_tokentype(cli_style[token])]
-        except AttributeError as err:
-            custom_styles[string_to_tokentype(token)] = cli_style[token]
+    custom_styles = dict([(string_to_tokentype(x), y)
+                            for x, y in cli_style.items()])
+
     return PygmentsStyle.from_defaults(style_dict=custom_styles,
                                        pygments_style_cls=style)
