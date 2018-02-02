d3c59f312a9ddad301a919b3d87476f8dd1a8eb5
Jonathan Slenders <jonathan@slenders.be>
2018-02-03 00:17:29 +0100

    Include default Pygments style in print_formatted_text.

--- prompt_toolkit/shortcuts/utils.py
+++ prompt_toolkit/shortcuts/utils.py
@@ -3,7 +3,7 @@ from prompt_toolkit.formatted_text import to_formatted_text
 from prompt_toolkit.output.base import Output
 from prompt_toolkit.output.defaults import create_output, get_default_output
 from prompt_toolkit.renderer import print_formatted_text as renderer_print_formatted_text
-from prompt_toolkit.styles import default_ui_style, BaseStyle, merge_styles
+from prompt_toolkit.styles import default_ui_style, default_pygments_style, BaseStyle, merge_styles
 import six
 
 __all__ = [
@@ -58,6 +58,8 @@ def print_formatted_text(*values, **kwargs):
     :param sep: String inserted between values, default a space.
     :param end: String appended after the last value, default a newline.
     :param style: :class:`.Style` instance for the color scheme.
+    :param include_default_pygments_style: `bool`. Include the default Pygments
+        style when set to `True` (the default).
     """
     # Pop kwargs (Python 2 compatibility).
     sep = kwargs.pop('sep', ' ')
@@ -66,19 +68,21 @@ def print_formatted_text(*values, **kwargs):
     flush = kwargs.pop('flush', False)
     style = kwargs.pop('style', None)
     output = kwargs.pop('output', None)
+    include_default_pygments_style = kwargs.pop('include_default_pygments_style', True)
     assert not kwargs
     assert not (output and file)
+    assert style is None or isinstance(style, BaseStyle)
 
-    # Other defaults.
-    if style is None:
-        merged_style = default_ui_style()
-    else:
-        assert isinstance(style, BaseStyle)
-        merged_style = merge_styles([
-            default_ui_style(),
-            style
-        ])
+    # Build/merge style.
+    styles = [default_ui_style()]
+    if include_default_pygments_style:
+        styles.append(default_pygments_style())
+    if style:
+        styles.append(style)
 
+    merged_style = merge_styles(styles)
+
+    # Create Output object.
     if output is None:
         if file:
             output = create_output(stdout=file)
