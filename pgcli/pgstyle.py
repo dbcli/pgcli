576f77b4982711993e37a6cee91fe8daac7346b4
Irina Truong <i.chernyavska@gmail.com>
2018-08-05 23:48:03 +0000

    By default, bottom toolbar styles are reversed. We don't want that.

--- pgcli/pgstyle.py
+++ pgcli/pgstyle.py
@@ -1,12 +1,11 @@
 from __future__ import unicode_literals
 
+import pygments.styles
 from pygments.token import string_to_tokentype
+from pygments.style import Style as PygmentsStyle
 from pygments.util import ClassNotFound
 from prompt_toolkit.styles.pygments import style_from_pygments_cls, style_from_pygments_dict
-from prompt_toolkit.styles import merge_styles
-import pygments.styles
-
-from pygments.style import Style
+from prompt_toolkit.styles import merge_styles, Style
 
 
 def style_factory(name, cli_style):
@@ -23,9 +22,11 @@ def style_factory(name, cli_style):
         except AttributeError as err:
             custom_styles[string_to_tokentype(token)] = cli_style[token]
 
+    override_style = Style([('bottom-toolbar', 'noreverse')])
     return merge_styles([
         style_from_pygments_cls(style),
         style_from_pygments_dict(custom_styles),
+        override_style
     ])
 
 
@@ -43,7 +44,7 @@ def style_factory_output(name, cli_style):
             style.update(
                 {string_to_tokentype(token): cli_style[token], })
 
-    class OutputStyle(pygments.style.Style):
+    class OutputStyle(PygmentsStyle):
         default_style = ""
         styles = style
 
