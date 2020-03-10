b1c9571dec2c2bfc90075b6f7b9db267a138bc75
laixintao <laixintaoo@gmail.com>
2020-03-10 12:51:50 +0800

    delete 3.5 from setup.py

--- setup.py
+++ setup.py
@@ -48,7 +48,6 @@ setup(
         "Operating System :: Unix",
         "Programming Language :: Python",
         "Programming Language :: Python :: 3",
-        "Programming Language :: Python :: 3.5",
         "Programming Language :: Python :: 3.6",
         "Programming Language :: Python :: 3.7",
         "Programming Language :: SQL",
