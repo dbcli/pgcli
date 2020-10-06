3246eb33ea30bf0a92e3eebf77a1d013c7fee05a
laixintao <laixintaoo@gmail.com>
2020-10-06 09:42:03 +0800

    test: test pgcli on python3.9

--- setup.py
+++ setup.py
@@ -52,6 +52,7 @@ setup(
         "Programming Language :: Python :: 3.6",
         "Programming Language :: Python :: 3.7",
         "Programming Language :: Python :: 3.8",
+        "Programming Language :: Python :: 3.9",
         "Programming Language :: SQL",
         "Topic :: Database",
         "Topic :: Database :: Front-Ends",
