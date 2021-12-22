3432262e1e23806e8a3e3af7b70cc286df04bd64
Sweenu <brunoinec@gmail.com>
2021-12-22 11:14:03 +0100

    Add initial sshtunnel support

--- setup.py
+++ setup.py
@@ -39,7 +39,10 @@ setup(
     description=description,
     long_description=open("README.rst").read(),
     install_requires=install_requirements,
-    extras_require={"keyring": ["keyring >= 12.2.0"]},
+    extras_require={
+        "keyring": ["keyring >= 12.2.0"],
+        "sshtunnel": ["sshtunnel >= 0.4.0"]
+    },
     python_requires=">=3.6",
     entry_points="""
         [console_scripts]
