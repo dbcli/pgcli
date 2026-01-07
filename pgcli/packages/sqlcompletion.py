a6122c49ca153c4de9a4db2b71e9536c870014dd
godtechwak <godtechwak@gmail.com>
2026-01-08 01:55:05 +0900

    Add \nq to named query command autocomplete

--- pgcli/packages/sqlcompletion.py
+++ pgcli/packages/sqlcompletion.py
@@ -270,7 +270,7 @@ def suggest_special(text):
                 return (Schema(), Function(schema=None, usage="special"))
             return (Schema(), rel_type(schema=None))
 
-    if cmd in ["\\n", "\\ns", "\\nd"]:
+    if cmd in ["\\n", "\\ns", "\\nd", "\\nq"]:
         return (NamedQuery(),)
 
     return (Keyword(), Special())
